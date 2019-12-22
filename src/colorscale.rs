2cc04f38784c5bca094c28379535b191d12044f9
Fernando Herrero Carrón <elferdo@gmail.com>
2019-12-22 22:58:48 +0100

    Choose interval from percentage

--- src/colorscale.rs
+++ src/colorscale.rs
@@ -4,43 +4,45 @@ struct Palette{
     breakpoints: Vec<RGB8>
 }
 
-fn interval<T>(v: &Vec<T>, percentage: usize) -> Result<(&T, &T), ()> {
-    // if v.is_empty() || percentage > 100 {
-    // 	return Err(());
-    // }
-    
-    if v.len() == 1 {
-	Ok((&v[0], &v[0]))
-    } else {
-	let _interval_length = 100f64 / (v.len() as f64);
-	
-	let point = v.len() * 100 / percentage;
-
-	return Ok((&v[point], &v[point + 1]));
-    }
-}
-
 impl Palette {
     fn new(breakpoints: Vec<RGB8>) -> Self {
 	Palette {breakpoints: breakpoints}
     }
 
-    fn color(&self, _percentage: u8) -> RGB8 {
+    fn color(&self, _percentage: usize) -> RGB8 {
 	if self.breakpoints.len() == 1 {
-	    self.breakpoints[0]
-	}
-	else if self.breakpoints.len() == 2{
-	    let rgb0 = self.breakpoints[0];
-	    let diff = self.breakpoints[1] - self.breakpoints[0];
+	    return self.breakpoints[0];
+	};
 
-	    RGB8{r: rgb0.r + diff.r / 2,
-		 g: rgb0.g + diff.g / 2,
-		 b: rgb0.b + diff.b / 2}
-	} else {
-	    RGB8{r: 0, g: 0, b: 0}
-	}
+	let (a, b) = interval(&self.breakpoints, _percentage).unwrap();
+
+	RGB8{r: (a.r + b.r) / 2,
+	     g: (a.g + b.g) / 2,
+	     b: (a.b + b.b) / 2}
     }
 }
+
+fn interval<T>(v: &Vec<T>, mut percentage: usize) -> Result<(&T, &T), ()> {
+    if v.is_empty() || percentage > 100 {
+    	return Err(());
+    }
+    
+    if v.len() == 1 {
+	Ok((&v[0], &v[0]))
+    } else {
+	// Little trick to close the last interval on the right
+	if percentage == 100 {
+	    percentage -= 1;
+	}
+	let intervals = v.len() - 1;
+
+	let point = intervals * percentage / 100;
+	dbg!(point);
+
+	return Ok((&v[point], &v[point + 1]));
+    }
+}
+
 
 #[cfg(test)]
 mod test {
@@ -79,7 +81,7 @@ mod test {
 	let target_color2 = RGB8{r:2, g:2, b:2};
 	
 	let palette = Palette::new(vec![target_color0, target_color1, target_color2]);
-	assert_eq!(palette.color(50), RGB8{r:1, g:1, b:1});
+	assert_eq!(palette.color(50), target_color1);
     }
 
     #[test]
@@ -102,5 +104,19 @@ mod test {
 	
 	assert_eq!(interval(&v, 50), Ok((&v[1], &v[2])));
     }
+
+    #[test]
+    // All intervals are [a, b) except the last one [n, 100]
+    fn interval_when_three_points_then_100_equal_second_interval() {
+	let v = vec![0, 1, 2];
+	
+	assert_eq!(interval(&v, 100), Ok((&v[1], &v[2])));
+    }
     
+    #[test]
+    fn interval_when_four_points_then_75_equal_third_interval() {
+	let v = vec![0, 1, 2, 3];
+	
+	assert_eq!(interval(&v, 75), Ok((&v[2], &v[3])));
+    }
 }
